ae6bf1b45a7c4ef0c4e04bb88d3f0b874b1a8a0f
xianhetian <xianhetian>
2019-11-17 01:55:38 +0800

    Wiki.

--- assets/js/jquery.toc.js
+++ assets/js/jquery.toc.js
@@ -1,5 +1,5 @@
 // https://github.com/ghiculescu/jekyll-table-of-contents
-// Updated by http://mazhuang.org
+// Updated by http://leaf0214.github.io
 (function($){
   $.fn.toc = function(options) {
     var defaults = {
